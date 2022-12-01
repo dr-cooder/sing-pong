d22a3853f396c07d830dfa78dca99ac8cc4ab3e6
Felix Warner (RIT Student) <fgw7468@rit.edu>
2022-12-01 12:13:14 -0500

    Added Readme and FFT code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sing-Pong
+## Premise
+The gameplay is akin to that of Pong, albeit more like squash in that the player controls a paddle bounces a ball against an opposite wall as many times as they can before dropping it. This paddle is controlled by voice, hence the name of the project. Players are to sing into a wooden spoon with an attached microphone, the reading of which is to be interpreted by an Arduino setup as a pitch and sent to the Unity application via serial. The higher the pitch, the higher the paddle.
+## Sources
+- In-class Arduino Mic FFT example
+- In-class Arduino-Unity communication exercises
--- SingPongArduino/SingPongArduino.ino
+++ SingPongArduino/SingPongArduino.ino
@@ -1,9 +1,69 @@
-void setup() {
-  // put your setup code here, to run once:
+// arduinoFFT - Version: 1.5.6
+#include <arduinoFFT.h>//use app Library Mgr to hunt and add this
 
+arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
+/*
+  These values can be changed in order to evaluate the functions
+*/
+#define CHANNEL A1
+// int speakPin = 9;
+
+const uint16_t samples = 256;//128; //64; //This value MUST ALWAYS be a power of 2
+const double samplingFrequency = 5000; //Hz, must be less than 10000 due to ADC
+
+unsigned int sampling_period_us;
+unsigned long microseconds;
+
+/*
+  These are the input and output vectors
+  Input vectors receive computed results from FFT
+*/
+double vReal[samples];
+double vImag[samples];
+
+#define SCL_INDEX 0x00
+#define SCL_TIME 0x01
+#define SCL_FREQUENCY 0x02
+#define SCL_PLOT 0x03
+
+void setup()
+{
+  sampling_period_us = round(1000000 * (1.0 / samplingFrequency));
+  Serial.begin(115200);
+  //Serial.println("Ready");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  /*SAMPLING*/
+  for (int i = 0; i < samples; i++)
+  {
+    microseconds = micros();    //Overflows after around 70 minutes!
 
+    vReal[i] = analogRead(CHANNEL);
+    vImag[i] = 0;
+    while (micros() < (microseconds + sampling_period_us)) {
+      //empty loop
+    }
+  }
+
+  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
+  FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
+  FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
+  
+  double peakFreq = FFT.MajorPeak(vReal, samples, samplingFrequency); /* peak freq? */
+  
+  //calculate a sort-of max magnitude from 1/4, 1/3, and 1/2 thru array of magnitudes
+  long sortaMag = max(vReal[int(samples * .33)], vReal[int(samples * .5)]);
+  sortaMag = max(sortaMag, vReal[int(samples * .25)]);
+  Serial.print(sortaMag);
+  Serial.print(" - ");
+  
+  Serial.println(peakFreq, 6); //Print out what frequency is the most dominant.
+  
+  // if (sortaMag > 300) { //If this sample was loud enough
+  //   tone(speakPin, peakFreq, 800); //send out to speaker pin 
+  //   delay(800); //wait for tone
+  // }
+  delay(2);
 }
